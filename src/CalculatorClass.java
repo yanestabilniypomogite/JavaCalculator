b9a9c5e5f00940d4ac65f536bfa3485f36e2cd9d
Vladislav Borisov <93284707+yanestabilniypomogite@users.noreply.github.com>
2024-01-17 19:30:25 +0300

    Add files via upload

--- src/CalculatorClass.java
+++ src/CalculatorClass.java
@@ -1,25 +1,75 @@
-import static java.lang.System.in;
+import java.util.Scanner;
+import static java.lang.System.exit;
 
 public class CalculatorClass {
-    public static void SumMethod(int a, int b) {
-        System.out.println("Ответ:\n" + (a + b));
+    public static Scanner in = new Scanner(System.in);
+    public static int firstNum;
+    public static int secondNum;
+    public static boolean retry = true;
+    public static int num;
+
+    public static void inputNumbers() {
+        System.out.println("Введите первое число:");
+        firstNum = inputCheck();
+        System.out.println("Введите второе число:");
+        secondNum = inputCheck();
     }
-    public static void DiffMethod(int a, int b) {
-        System.out.println("Ответ:\n" + (a - b));
+
+    public static int inputCheck() {
+            try {
+                num = in.nextInt();
+            } catch (Exception e) {
+                System.out.println("Некорректные данные. На ввод разрешены только цифры!");
+                exit(1);
+            }
+        return num;
     }
-    public static void MultMethod(int a, int b) {
-        System.out.println("Ответ:\n" + (a * b));
+    public static void sumMethod() {
+        inputNumbers();
+        System.out.println("Ответ:\n" + (firstNum + secondNum));
     }
-    public static void DivMethod(double a, double b) {
-        System.out.println("Ответ:\n" + (a / b));
+
+    public static void diffMethod() {
+        inputNumbers();
+        System.out.println("Ответ:\n" + (firstNum - secondNum));
     }
-    public static boolean IsHonest(int a) {
-        int remainder = a % 2;
-        boolean isHonest;
-        isHonest = remainder == 0;
-        return isHonest;
+
+    public static void multMethod() {
+        inputNumbers();
+        System.out.println("Ответ:\n" + (firstNum * secondNum));
     }
-    public static void  RetryOffer() {
+
+    public static void divMethod() {
+        inputNumbers();
+        System.out.println("Ответ:\n" + ((double) firstNum / (double) secondNum));
+    }
+
+    public static void isEven() {
+        int evenNum;
+        boolean isEven;
+        System.out.println("Введите число для проверки:");
+        evenNum = inputCheck();
+        isEven = evenNum % 2 == 0;
+        if (isEven) {
+            System.out.println("Число " + evenNum + " четное");
+        } else {
+            System.out.println("Число " + evenNum + " нечетное");
+        }
+    }
+
+    public static boolean retryOffer() {
         System.out.println("Хотите попробовать другие операции с разными числами?\n 1.Да\n 2.Нет");
+        while (retry) {
+                int yesOrNo = inputCheck();
+                if (yesOrNo != 2 & yesOrNo != 1) {
+                    System.out.println("Введите 1(да) или 2(нет):");
+                } else if (yesOrNo == 1) {
+                    break;
+                } else {
+                    System.out.println("До свидания!");
+                    retry = false;
+                }
+            }
+        return retry;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,141 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean retry = true;
+        int choice;
         Scanner in = new Scanner(System.in);
-        boolean isWork = true;
-        int yesOrNo;
-        int firstNum;
-        int secondNum;
-        int honestNum;
-        int chois;
-        while (isWork) {
-            System.out.println("\nВыберите одну из операций(Введите число от 1 до 6):\n 1.Сложение\n 2.Вычитание\n 3.Умножение\n 4.Деление\n 5.Проверка на четность\n 6.Выход");
-            chois = in.nextInt();
-            if (chois > 0 & chois < 7) {
-                if (chois == 5) {
-                    System.out.println("Введите число для проверки:");
-                    honestNum = in.nextInt();
-                    if (CalculatorClass.IsHonest(honestNum)) {
-                        System.out.println("Число " + honestNum + " четное");
-                        CalculatorClass.RetryOffer();
-                        yesOrNo = in.nextInt();
-                        while (yesOrNo != 1 || yesOrNo != 2) {
-                            if (yesOrNo == 1) {
-                                break;
-                            } else if (yesOrNo == 2) {
-                                System.out.println("До свидания!");
-                                isWork = false;
-                                break;
-                            } else {
-                                System.out.println("Введите 1(да) или 2(нет):");
-                                yesOrNo = in.nextInt();
-                            }
-                        }
-                    } else {
-                        System.out.println("Число " + honestNum + " нечетное");
-                        CalculatorClass.RetryOffer();
-                        yesOrNo = in.nextInt();
-                        while (yesOrNo != 1 || yesOrNo != 2) {
-                            if (yesOrNo == 1) {
-                                break;
-                            } else if (yesOrNo == 2) {
-                                System.out.println("До свидания!");
-                                isWork = false;
-                                break;
-                            } else {
-                                System.out.println("Введите 1(да) или 2(нет):");
-                                yesOrNo = in.nextInt();
-                            }
-                        }
-                    }
-                } else if (chois == 1) {
-                    System.out.println("Введите первое число:");
-                    firstNum = in.nextInt();
-                    System.out.println("Введите второе число:");
-                    secondNum = in.nextInt();
-                    CalculatorClass.SumMethod(firstNum, secondNum);
-                    CalculatorClass.RetryOffer();
-                    yesOrNo = in.nextInt();
-                    while (yesOrNo != 1 || yesOrNo != 2) {
-                        if (yesOrNo == 1) {
-                            break;
-                        } else if (yesOrNo == 2) {
-                            System.out.println("До свидания!");
-                            isWork = false;
-                            break;
-                        } else {
-                            System.out.println("Введите 1(да) или 2(нет):");
-                            yesOrNo = in.nextInt();
-                        }
-                    }
-                } else if (chois == 2) {
-                    System.out.println("Введите первое число:");
-                    firstNum = in.nextInt();
-                    System.out.println("Введите второе число:");
-                    secondNum = in.nextInt();
-                    CalculatorClass.DiffMethod(firstNum, secondNum);
-                    CalculatorClass.RetryOffer();
-                    yesOrNo = in.nextInt();
-                    while (yesOrNo != 1 || yesOrNo != 2) {
-                        if (yesOrNo == 1) {
-                            break;
-                        } else if (yesOrNo == 2) {
-                            System.out.println("До свидания!");
-                            isWork = false;
-                            break;
-                        } else {
-                            System.out.println("Введите 1(да) или 2(нет):");
-                            yesOrNo = in.nextInt();
-                        }
-                   }
-                } else if (chois == 3) {
-                    System.out.println("Введите первое число:");
-                    firstNum = in.nextInt();
-                    System.out.println("Введите второе число:");
-                    secondNum = in.nextInt();
-                    CalculatorClass.MultMethod(firstNum, secondNum);
-                    CalculatorClass.RetryOffer();
-                    yesOrNo = in.nextInt();
-                    while (yesOrNo != 1 || yesOrNo != 2) {
-                        if (yesOrNo == 1) {
-                            break;
-                        } else if (yesOrNo == 2) {
-                            System.out.println("До свидания!");
-                            isWork = false;
-                            break;
-                        } else {
-                            System.out.println("Введите 1(да) или 2(нет):");
-                            yesOrNo = in.nextInt();
-                        }
-                    }
-                } else if (chois == 4) {
-                    System.out.println("Введите первое число:");
-                    firstNum = in.nextInt();
-                    System.out.println("Введите второе число:");
-                    secondNum = in.nextInt();
-                    CalculatorClass.DivMethod(firstNum, secondNum);
-                    CalculatorClass.RetryOffer();
-                    yesOrNo = in.nextInt();
-                    while (yesOrNo != 1 || yesOrNo != 2) {
-                        if (yesOrNo == 1) {
-                            break;
-                        } else if (yesOrNo == 2) {
-                            System.out.println("До свидания!");
-                            isWork = false;
-                            break;
-                        } else {
-                            System.out.println("Введите 1(да) или 2(нет):");
-                            yesOrNo = in.nextInt();
-                        }
-                    }
-                } else if (chois == 6) {
+        while (retry) {
+            System.out.println("Выберите одну из операций(Введите число от 1 до 6):\n 1.Сложение\n 2.Вычитание\n 3.Умножение\n 4.Деление\n 5.Проверка на четность\n 6.Выход");
+            choice = CalculatorClass.inputCheck();
+            if (choice > 0 & choice < 7) {
+                if (choice == 5) {
+                   CalculatorClass.isEven();
+                   retry = CalculatorClass.retryOffer();
+                } else if (choice == 1) {
+                    CalculatorClass.sumMethod();
+                    retry = CalculatorClass.retryOffer();
+                } else if (choice == 2) {
+                    CalculatorClass.diffMethod();
+                    retry = CalculatorClass.retryOffer();
+                } else if (choice == 3) {
+                    CalculatorClass.multMethod();
+                    retry = CalculatorClass.retryOffer();
+                } else if (choice == 4) {
+                    CalculatorClass.divMethod();
+                    retry = CalculatorClass.retryOffer();
+                } else if (choice == 6) {
                     System.out.println("До свидания!");
-                    isWork = false;
-                    break;
+                    retry = false;
                 }
-            } else {
-                System.out.println("Введите значение от 1 до 6!");
+            }  else {
+                    System.out.println("Введите значение от 1 до 6!");
+                }
             }
         }
     }
-}
+
